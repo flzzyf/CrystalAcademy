9167ad718cc495e504f8e4f9f1dc9ce2af22912c
Flzzyf <flzzyf076@gmail.com>
2018-09-28 08:35:56 +0800

    导进图片分割器

---
+++ CrystalAcademy/Assets/Import/Block.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour 
+{
+    public Vector2Int pos;
+
+	public void Init(Vector2Int _pos, Texture2D _image)
+    {
+        pos = _pos;
+
+        GetComponent<MeshRenderer>().material.mainTexture = _image;
+        GetComponent<MeshRenderer>().material.shader = Shader.Find("Unlit/Transparent");
+    }
+}
---
+++ CrystalAcademy/Assets/Import/ImageSlicer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ImageSlicer
+{
+    public static Texture2D[,] Slice(Texture2D _texture, int _size)
+    {
+        int imageSize = Mathf.Min(_texture.width, _texture.height);
+        int blockSize = imageSize / _size;
+
+        Texture2D[,] blocks = new Texture2D[_size, _size];
+
+        for (int y = 0; y < _size; y++)
+        {
+            for (int x = 0; x < _size; x++)
+            {
+                Texture2D block = new Texture2D(blockSize, blockSize);
+
+                block.wrapMode = TextureWrapMode.Clamp;
+                block.filterMode = FilterMode.Point;
+                block.SetPixels(_texture.GetPixels(x * blockSize, y * blockSize, blockSize, blockSize));
+                block.Apply();
+                blocks[x, y] = block;
+            }
+        }
+
+        return blocks;
+    }
+}
